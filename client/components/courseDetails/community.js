1ebe8f0193329a465e4a40145111391172d93291
prince3339 <prince3339@ymail.com>
2016-10-11 16:05:24 +0600

    project setup

--- client/components/courseDetails/community.js
+++ client/components/courseDetails/community.js
@@ -1,16 +1,17 @@
 Template.community.onRendered(function() {
-  $(document).ready(function() {
-    if (Framework7.prototype.device.android) {
-      $('.eddy-tabs').addClass('margin-top-115');
-    }else {
-      $('.eddy-tabs').addClass('margin-top-110');
-    }
+      $(document).ready(function() {
+        if(Framework7.prototype.device.android) {
+          $('.eddy-tabs').addClass('margin-top-115');
+        }else {
+          $('.eddy-tabs').addClass('margin-top-110');
+        }
+      })
   });
 
 Template.community.helpers({
 chapters(){
   return Courses.find();
-  },
+  }
 });
 
 Template.community.events({
