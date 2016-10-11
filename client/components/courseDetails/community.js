d0aa25ff7661b5ff210200543bc56c9b03818d46
prince3339 <prince3339@ymail.com>
2016-10-12 01:19:52 +0600

    console log removed

--- client/components/courseDetails/community.js
+++ client/components/courseDetails/community.js
@@ -1,11 +1,9 @@
 Template.community.onRendered(function() {
   $(document).ready(function() {
     $('.eddy-comment-btn').click(function() {
-      console.log(this);
       $(this).parent().parent().parent().siblings('.eddy-community--post--comment-section').removeClass('hide');
     });
     $('.eddy-comment-reply-btn').click(function() {
-      console.log(this);
       $(this).parent().parent().parent().siblings('.eddy-community--post--comments--reply').removeClass('hide');
     });
   })
