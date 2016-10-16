8ff9ef4c8435d41c95fa1c2fa8dacf9f1fdc2914
prince3339 <prince3339@ymail.com>
2016-10-16 16:12:32 +0600

    quiz page style added

---
+++ client/components/quiz-page/quiz.js
@@ -0,0 +1,22 @@
+Template.quiz.events({
+  "click .eddy-quiz--option-btn": function(event, template){
+    event.stopPropagation();
+    $('.toggle-opacity').addClass('opacity-20');
+    $('.eddy-quiz--option').css('top', '0');
+  },
+
+  "click .eddy-quiz--option--close": function(event, template){
+    $('.eddy-quiz--option').css({'top': '-120px', 'transition': 'all 0.3s'});
+    $('.toggle-opacity').removeClass('opacity-20');
+  },
+
+  "click .eddy-quiz--option--ok": function(event, template){
+    $('.eddy-quiz--option').css({'top': '-120px', 'transition': 'all 0.3s'});
+    $('.toggle-opacity').removeClass('opacity-20');
+  },
+
+  "click .toggle-opacity": function(event, template){
+    $('.eddy-quiz--option').css({'top': '-120px', 'transition': 'all 0.3s'});
+    $('.toggle-opacity').removeClass('opacity-20');
+  }
+});
