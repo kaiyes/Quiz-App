b2ee62d20a587079fe89e81c5682ab3686016406
prince3339 <prince3339@ymail.com>
2016-10-17 02:23:06 +0600

    quiz result page design ongoing

---
+++ client/components/quiz-page/quiz-result.js
@@ -0,0 +1,14 @@
+Template.quizResult.onRendered(function() {
+  $(document).ready(function	(){
+    $(function(){
+      var $ppc = $('.eddy-progress--wrapper'),
+        percent = parseInt($ppc.data('percent')),
+        deg = 360*percent/100;
+      if (percent > 50) {
+        $ppc.addClass('gt-50');
+      }
+      $('.eddy-progress--bar--fill').css('transform','rotate('+ deg +'deg)');
+      $('.eddy-progress--percents span').html(percent+' %');
+    });
+  });
+});
