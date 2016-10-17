cf6ffaa78ad818cdc017b873bf3c9abf824878a5
prince3339 <prince3339@ymail.com>
2016-10-18 00:18:59 +0600

    quiz result page ui added

--- client/components/quiz-page/quiz.js
+++ client/components/quiz-page/quiz.js
@@ -18,5 +18,22 @@ Template.quiz.events({
   "click .toggle-opacity": function(event, template){
     $('.eddy-quiz--option').css({'top': '-120px', 'transition': 'all 0.3s'});
     $('.toggle-opacity').removeClass('opacity-20');
+  },
+
+  "click .eddy--sqr-buttons__price": function(event, template) {
+    event.preventDefault();
+    Router.go('/quiz-result');
+  },
+  "click .eddy--sqr-buttons__plan": function(event, template) {
+    event.preventDefault();
+    Router.go('/quiz-result');
+  },
+  "click .eddy--sqr-buttons__place": function(event, template) {
+    event.preventDefault();
+    Router.go('/quiz-result');
+  },
+  "click .eddy--sqr-buttons__product": function(event, template) {
+    event.preventDefault();
+    Router.go('/quiz-result');
   }
 });
