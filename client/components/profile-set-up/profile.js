4cc7598b299ba9bb86d516b1c0795aa8959d7ec5
prince3339 <prince3339@ymail.com>
2016-10-24 00:44:07 +0600

    profile page style added

--- client/components/profile-set-up/profile.js
+++ client/components/profile-set-up/profile.js
@@ -1,5 +1,22 @@
+Template.profile.onRendered(function() {
+  $(document).ready(function() {
+    $(".eddy-profile .form-group:nth-of-type(3) select option:nth-child(1)").html('University *');
+    $(".eddy-profile .form-group:nth-of-type(5) select option:nth-child(1)").html('Nickname');
+    $(".eddy-profile .form-group:nth-of-type(7) select option:nth-child(1)").html('Country');
+    //$('.eddy-profile .form-group:nth-of-type(1) .afCloudinary button').html('<i class="fa fa-plus" aria-hidden="true"></i>');
+    //$('.eddy-profile .form-group:nth-of-type(1) .afCloudinary .js-remove').html('<i class="fa fa-close" aria-hidden="true"></i>');
+  })
+});
+
 Template.profile.events({
   "click #foo": function(event, template){
 
+  },
+  "click .eddy-profile .form-group:nth-of-type(1) .afCloudinary button": function(event, template) {
+    //$('.eddy-profile .form-group:nth-of-type(1) .afCloudinary .js-remove').html('<i class="fa fa-close" aria-hidden="true"></i>');
+  }
+  ,
+  ".eddy-profile .form-group:nth-of-type(1) .afCloudinary .js-remove": function(event, template) {
+    //$('.eddy-profile .form-group:nth-of-type(1) .afCloudinary button').html('<i class="fa fa-plus" aria-hidden="true"></i>');
   }
 });
