26fabbd2a5e773d4ed5dd91bc06d55537759456f
prince3339 <prince3339@ymail.com>
2016-10-24 00:55:27 +0600

    submit text changed to DONE using js

--- client/components/profile-set-up/profile.js
+++ client/components/profile-set-up/profile.js
@@ -3,7 +3,7 @@ Template.profile.onRendered(function() {
     $(".eddy-profile .form-group:nth-of-type(3) select option:nth-child(1)").html('University *');
     $(".eddy-profile .form-group:nth-of-type(5) select option:nth-child(1)").html('Nickname');
     $(".eddy-profile .form-group:nth-of-type(7) select option:nth-child(1)").html('Country');
-    //$('.eddy-profile .form-group:nth-of-type(1) .afCloudinary button').html('<i class="fa fa-plus" aria-hidden="true"></i>');
+    $('.eddy-profile .form-group:nth-of-type(8) button').html('DONE');
     //$('.eddy-profile .form-group:nth-of-type(1) .afCloudinary .js-remove').html('<i class="fa fa-close" aria-hidden="true"></i>');
   })
 });
