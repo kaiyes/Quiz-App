46b12057f274c5f1064ea63bda0a2882a4617c88
prince3339 <prince3339@ymail.com>
2016-10-24 23:32:52 +0600

    profile form mobile keyboard overlapping issue solved

--- client/components/profile-set-up/profile.js
+++ client/components/profile-set-up/profile.js
@@ -16,7 +16,37 @@ Template.profile.events({
     //$('.eddy-profile .form-group:nth-of-type(1) .afCloudinary .js-remove').html('<i class="fa fa-close" aria-hidden="true"></i>');
   }
   ,
-  ".eddy-profile .form-group:nth-of-type(1) .afCloudinary .js-remove": function(event, template) {
+  "click .eddy-profile .form-group:nth-of-type(1) .afCloudinary .js-remove": function(event, template) {
     //$('.eddy-profile .form-group:nth-of-type(1) .afCloudinary button').html('<i class="fa fa-plus" aria-hidden="true"></i>');
+  },
+  "click .form-group:nth-of-type(2)": function(event, template) {
+      $(".page-content").animate({
+            scrollTop: $('.form-group:nth-of-type(2)').height() * $('.form-group:nth-of-type(2)').index()
+          },"slow");
+  },
+  "click .form-group:nth-of-type(3)": function(event, template) {
+      $(".page-content").animate({
+            scrollTop: $('.form-group:nth-of-type(3)').height() * 3
+          },"slow");
+  },
+  "click .form-group:nth-of-type(4)": function(event, template) {
+      $(".page-content").animate({
+            scrollTop: $('.form-group:nth-of-type(4)').height() * 4
+          },"slow");
+  },
+  "click .form-group:nth-of-type(5)": function(event, template) {
+      $(".page-content").animate({
+            scrollTop: $('.form-group:nth-of-type(5)').height() * 5
+          },"slow");
+  },
+  "click .form-group:nth-of-type(6)": function(event, template) {
+      $(".page-content").animate({
+            scrollTop: $('.form-group:nth-of-type(6)').height() * 6
+          },"slow");
+  },
+  "click .form-group:nth-of-type(7)": function(event, template) {
+      $(".page-content").animate({
+            scrollTop: $('.form-group:nth-of-type(7)').height() * 7
+          },"slow");
   }
 });
