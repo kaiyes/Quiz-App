49ed6e161233458aa4c7982a2675e6409f4654ff
prince3339 <prince3339@ymail.com>
2016-11-02 02:20:24 +0600

    toolbar ui issue solved

--- client/components/toolbar/toolbar.js
+++ client/components/toolbar/toolbar.js
@@ -5,3 +5,14 @@ Template.toolbar.events({
   },
 
 });
+
+Template.toolbar.onRendered(function() {
+  $(document).ready(function	(){
+    if (Framework7.prototype.device.android) {
+        $('.eddy-toolbar--icon__noti--badge').css('right', '1.5rem');
+      }
+      else {
+        $('.eddy-toolbar--icon__noti--badge').css('left', '3.2rem');
+      }
+  });
+});
