b2d124eb90f2cdc63231cdef23dce26545443cab
x <x@xs-iMac.local>
2016-11-08 14:28:58 +0600

    startup now populates course list after fresh install

--- client/main.js
+++ client/main.js
@@ -1,9 +1,12 @@
 Meteor.startup(function(){
    myApp = new Framework7({
-      materialRipple: true,
-      materialRippleElements: true,
       fastClicks: true
-  });
+    });
    $$ = Dom7;
 
+   var courseCount = Courses.find().count();
+   if (courseCount<=0) {
+     Courses.insert({ "courseName": "science", "chapters":['biology', 'maths']});
+     Courses.insert({ "courseName": "art", "chapters":['history', 'ভূগোল']})
+   }
 });
