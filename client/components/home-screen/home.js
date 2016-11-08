becb1c817d03af117f3ed9d41a1f4c60041dbcb7
x <x@xs-iMac.local>
2016-11-08 16:30:14 +0600

    clicking on the list rather than the name, routes to static pages in settings page

--- client/components/home-screen/home.js
+++ client/components/home-screen/home.js
@@ -1,6 +1,5 @@
 Template.homePage.events({
   "click #list": function(event, template){
-    console.log(this.toString());
     Session.set("topicName", this.toString());
     Router.go('/courseDetails');
  },
