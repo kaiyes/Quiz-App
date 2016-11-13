cde672217123f1f2a335486be848a952395bc7b6
x <x@xs-iMac.local>
2016-11-13 16:57:22 +0600

    added routing to opponent selection, challange opponent page shows right chapter name

--- client/components/challange-opponent/challange.js
+++ client/components/challange-opponent/challange.js
@@ -5,3 +5,15 @@ Template.challengeOpponent.onRendered(function() {
       $('.eddy-challenge-mrgn-top').addClass('margin-top-48');
     }
 });
+
+Template.challengeOpponent.helpers({
+  players: function(){
+    return Meteor.users.find();
+  }
+});
+
+Template.challengeOpponent.events({
+  "click #player": function(event, template){
+    console.log(Session.get('topicName'));
+  },
+});
--- client/components/courseDetails/chapters.js
+++ client/components/courseDetails/chapters.js
@@ -2,13 +2,14 @@ Template.chapters.helpers({
   chapters(){
     let courseName = Session.get('topicName');
     let chapters =  Courses.findOne({courseName:courseName});
-    console.log(chapters);
     return chapters;
   },
 });
 
 Template.chapters.events({
   "click #list": function(event, template){
-     console.log(this);
+     Session.set("chapter", this.toString())
+     Router.go('/challengeOpponent');
+    
   }
 });
