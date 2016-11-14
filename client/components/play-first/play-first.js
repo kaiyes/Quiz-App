694cbdb9f2ea13a3bfb96de1df947b72dde9812d
x <x@xs-iMac.local>
2016-11-14 14:06:32 +0600

    added timer in the oppenet select waiting page :rage:

--- client/components/play-first/play-first.js
+++ client/components/play-first/play-first.js
@@ -1,12 +1,23 @@
+var countdown = new ReactiveCountdown(120);
+
+Template.playFirst.onRendered(function(){
+  countdown.start(function() {
+    console.log("works");
+  });
+});
 
 Template.playFirst.helpers({
   userInfo: function(){
-     return Session.get('playerInfo');
+    return Session.get('playerInfo');
   },
-}); 
+
+  time:function(event, instance){
+   return countdown.get();
+  },
+});
 
 Template.playFirst.events({
-  "click #foo": function(event, template){
-
+  "click #play": function(event, instance){
+    console.log(countdown.get());
   }
 });
