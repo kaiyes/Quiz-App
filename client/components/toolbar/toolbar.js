199c35c82b56ca19a01e2afa9d65bdc7656eab75
sukun <sukun@sukuns-iMac.local>
2016-11-25 22:52:37 +0600

    toolbar ui issue fixe as well as style improved

--- client/components/toolbar/toolbar.js
+++ client/components/toolbar/toolbar.js
@@ -8,7 +8,7 @@ Template.toolbar.events({
 Template.toolbar.onRendered(function() {
 
     if (Framework7.prototype.device.android) {
-        $('.eddy-toolbar--icon__noti--badge').css('right', '2rem');
+        $('.eddy-toolbar--icon__noti--badge').css({'right': '1rem', 'top': '-1rem'});
       }
       else {
         $('.eddy-toolbar--icon__noti--badge').css('left', '3.2rem');
