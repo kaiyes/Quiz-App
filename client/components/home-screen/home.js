8a0f2f519506e732de1e453d3952c5960d940ae8
sukun <sukun@sukuns-iMac.local>
2016-12-16 02:03:24 +0600

    ui fixes

--- client/components/home-screen/home.js
+++ client/components/home-screen/home.js
@@ -8,6 +8,7 @@ Template.homePage.events({
    $('#showProfileInfo').hide();
    $('.eddy-home--profile-info').css({'margin-top': '0', 'transition': 'all 0.3s'});
    $('.eddy-home--quizes').removeClass('margin-v-70').addClass('margin-bottom-285');
+
  },
  "click #hideProfileInfo": function(event, template) {
    $('#showProfileInfo').slideDown();
