6c7ddd44d8bf10e2f4ce2033ae612003c7bfbce2
sukun <sukun@sukuns-iMac.local>
2016-12-20 00:48:21 +0600

    profile form changed with framework 7 form elements

--- client/components/profile-set-up/profile.js
+++ client/components/profile-set-up/profile.js
@@ -1,18 +1,5 @@
 Template.profile.onRendered(function() {
-  $('.eddy-profile .form-group:not(.form-group:nth-of-type(1))').click(function() {
-    //console.log(this);
-    var outerHeight = 0;
-    $(this).prevAll('.form-group').each(function() {
-      outerHeight += $(this).outerHeight() + 10;
-      //console.log(outerHeight);
-      //console.log($(this).outerHeight());
-    });
-    //console.log("TEST");
 
-    $(".page-content").animate({
-      scrollTop: outerHeight + $(this).outerHeight()
-    },"slow");
-  });
 });
 
 Template.profile.events({
@@ -79,33 +66,34 @@ Template.profile.events({
     Router.go('/homePage');
   },
 
-  "click .eddy-profile .form-group:nth-of-type(1) .afCloudinary .js-remove": function(event, template) {
-    //$('.eddy-profile .form-group:nth-of-type(1) .afCloudinary button').html('<i class="fa fa-plus" aria-hidden="true"></i>');
-  },
-
-  "click .form-group:nth-of-type(2)": function(event, template) {
+  "click .p-form:nth-of-type(1)": function(event, template) {
       $(".page-content").animate({
-            scrollTop: $('.form-group:nth-of-type(2)').height() * $('.form-group:nth-of-type(2)').index()
+            scrollTop: $('.p-form:nth-of-type(1)').height() + 15
           },"slow");
   },
-  "click .form-group:nth-of-type(3)": function(event, template) {
+  "click .p-form:nth-of-type(2)": function(event, template) {
       $(".page-content").animate({
-            scrollTop: $('.form-group:nth-of-type(3)').height() * 3
+            scrollTop: $('.p-form:nth-of-type(2)').height() * 2
           },"slow");
   },
-  "click .form-group:nth-of-type(4)": function(event, template) {
+  "click .p-form:nth-of-type(3)": function(event, template) {
       $(".page-content").animate({
-            scrollTop: $('.form-group:nth-of-type(4)').height() * 4
+            scrollTop: $('.p-form:nth-of-type(3)').height() * 3 + 15
           },"slow");
   },
-  "click .form-group:nth-of-type(5)": function(event, template) {
+  "click .p-form:nth-of-type(4)": function(event, template) {
       $(".page-content").animate({
-            scrollTop: $('.form-group:nth-of-type(5)').height() * 5 + 10
+            scrollTop: $('.p-form:nth-of-type(4)').height() * 4 + 15
           },"slow");
   },
-  "click .form-group:nth-of-type(6)": function(event, template) {
+  "click .p-form:nth-of-type(5)": function(event, template) {
       $(".page-content").animate({
-            scrollTop: $('.form-group:nth-of-type(6)').height() * 6 + 10
+            scrollTop: $('.p-form:nth-of-type(5)').height() * 5 + 15
+          },"slow");
+  },
+  "click .p-form:nth-of-type(6)": function(event, template) {
+      $(".page-content").animate({
+            scrollTop: $('.p-form:nth-of-type(6)').height() * 6 + 15
           },"slow");
   },
 });
