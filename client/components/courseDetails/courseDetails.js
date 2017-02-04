200443891c3a72b708556449b3a407b19ce97f21
Mahbubur Rahman <pappu687@gmail.com>
2017-02-04 16:44:30 +0600

    More css changes in lot of views

--- client/components/courseDetails/courseDetails.js
+++ client/components/courseDetails/courseDetails.js
@@ -1,52 +1,52 @@
 Template.courseDetails.onRendered(function() {
     if (Framework7.prototype.device.android) {
-      $('.eddy-tabs').addClass('margin-top-105');
-    }else {
-      $('.eddy-tabs').addClass('margin-top-110');
+        $('.eddy-tabs').addClass('margin-top-105');
+    } else {
+        $('.eddy-tabs').addClass('margin-top-90');
     }
 });
 
 Template.courseDetails.helpers({
-  selectedCourse(){
-    return Session.get('topicName');
-  },
+    selectedCourse() {
+        return Session.get('topicName');
+    },
 
 });
 
 Template.courseDetails.events({
-  "click #openTopicList": function(event, template){
-    event.preventDefault();
-    $('.eddy-courseDetails__select-topic').addClass('opened');
-    $('.pages').addClass('opacity-50');
-    $('.toolbar').addClass('opacity-30');
-  },
-  "click #closeTopicList": function(event, template){
-    $('.eddy-courseDetails__select-topic').removeClass('opened');
-    $('.pages').removeClass('opacity-50');
-    $('.toolbar').removeClass('opacity-30');
-  },
-  "click .eddy-courseDetails__select-topic": function(event, Template) {
-    event.preventDefault();
-  },
-  "click .toolbar": function(event, Template) {
-    $('.eddy-courseDetails__select-topic').removeClass('opened');
-    $('.pages').removeClass('opacity-50');
-    $('.toolbar').removeClass('opacity-30');
-  },
-  "click .pages": function(event, Template) {
-    $('.eddy-courseDetails__select-topic').removeClass('opened');
-    $('.pages').removeClass('opacity-50');
-    $('.toolbar').removeClass('opacity-30');
-  },
+    "click #openTopicList": function(event, template) {
+        event.preventDefault();
+        $('.eddy-courseDetails__select-topic').addClass('opened');
+        $('.pages').addClass('opacity-50');
+        $('.toolbar').addClass('opacity-30');
+    },
+    "click #closeTopicList": function(event, template) {
+        $('.eddy-courseDetails__select-topic').removeClass('opened');
+        $('.pages').removeClass('opacity-50');
+        $('.toolbar').removeClass('opacity-30');
+    },
+    "click .eddy-courseDetails__select-topic": function(event, Template) {
+        event.preventDefault();
+    },
+    "click .toolbar": function(event, Template) {
+        $('.eddy-courseDetails__select-topic').removeClass('opened');
+        $('.pages').removeClass('opacity-50');
+        $('.toolbar').removeClass('opacity-30');
+    },
+    "click .pages": function(event, Template) {
+        $('.eddy-courseDetails__select-topic').removeClass('opened');
+        $('.pages').removeClass('opacity-50');
+        $('.toolbar').removeClass('opacity-30');
+    },
 
- "click #popUpList": function(event, template){
-   event.preventDefault();
-   console.log(this.courseName);
-   Session.set("topicName", this.courseName);
-   $('.eddy-courseDetails__select-topic').removeClass('opened');
-   $('.pages').removeClass('opacity-50');
-   $('.toolbar').removeClass('opacity-30');
-},
+    "click #popUpList": function(event, template) {
+        event.preventDefault();
+        console.log(this.courseName);
+        Session.set("topicName", this.courseName);
+        $('.eddy-courseDetails__select-topic').removeClass('opened');
+        $('.pages').removeClass('opacity-50');
+        $('.toolbar').removeClass('opacity-30');
+    },
 
 
 });
