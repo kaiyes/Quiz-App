df7e0de9a7d36d6cdcae0760c7adbccc4a517e52
x <x@xs-iMac.local>
2017-02-07 15:00:32 +0600

    chart now takes reactive data in both axis, but shows labels

--- client/components/chart/chart.js
+++ client/components/chart/chart.js
@@ -4,20 +4,26 @@ Template.chart.onRendered(function () {
   let courseName = Session.get('topicName');
   let playerCourses =  Meteor.user().profile.selectedCourses;
   let course = _.find(playerCourses, ['courseName', courseName]);
+  let label = Object.keys(course.accuracy);
 
   let data = {
-    labels: ['', '', '', '', '', '', ''],
+    labels: label,
     datasets: [{
-        label: 'My First dataset',
-        fillColor: "rgba(22, 222, 102, 0.2)",
+          label: 'My First dataset',
+          fillColor: "rgba(22, 222, 102, 0.2)",
           strokeColor: "rgba(223, 219, 219, 1)",
-          pointColor: "rgba(252, 245, 245, 1)",
+          pointColor: "rgba(105, 98, 98, 1)",
           pointStrokeColor: "#fff",
           pointHighlightFill: "#e4ecc5",
           pointHighlightStroke: "rgba(220,220,220,1)",
           data: course.accuracy,
         }],
   };
-  let myLineChart = new Chart(ctx).Line(data);
+
+  var options = {
+    scaleShowLabels: false,
+  };
+
+  let myLineChart = new Chart(ctx).Line(data, options);
 
 });
