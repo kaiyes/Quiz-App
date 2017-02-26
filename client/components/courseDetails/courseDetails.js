e094985b44c25706c8b448a423e8a9db55c51fe2
Mahbubur Rahman <pappu687@gmail.com>
2017-02-26 15:38:49 +0600

    Added hash redirect in the community details page.

--- client/components/courseDetails/courseDetails.js
+++ client/components/courseDetails/courseDetails.js
@@ -1,4 +1,7 @@
 Template.courseDetails.onRendered(function() {
+    if (!_.isEmpty(window.location.hash) && window.location.hash !== $(".subnavbar .active").attr("href")) {
+        myApp.showTab(window.location.hash);
+    }
     if (Framework7.prototype.device.android) {
         $('.eddy-tabs').addClass('margin-top-105');
     } else {
@@ -40,7 +43,7 @@ Template.courseDetails.events({
     },
 
     "click #popUpList": function(event, template) {
-        event.preventDefault();        
+        event.preventDefault();
         Session.set("topicName", this.courseName);
         $('.eddy-courseDetails__select-topic').removeClass('opened');
         $('.pages').removeClass('opacity-50');
