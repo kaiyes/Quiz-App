f6f10fc9e4e1b6138fad8dd02c0db6b96e75cad7
Mahbubur Rahman <pappu687@gmail.com>
2017-03-26 22:34:33 +0600

    Trying to fix image placeholders

--- client/components/courseDetails/stats.js
+++ client/components/courseDetails/stats.js
@@ -28,6 +28,19 @@ Feedback.profiles = {
     },
 };
 
+
+Handlebars.registerHelper ('truncate', function (str, len) {
+    if (str.length > len && str.length > 0) {
+        var new_str = str + " ";
+        new_str = str.substr (0, len);
+        new_str = str.substr (0, new_str.lastIndexOf(" "));
+        new_str = (new_str.length > 0) ? new_str : str.substr (0, len);
+
+        return new Handlebars.SafeString ( new_str +'...' ); 
+    }
+    return str;
+});
+
 Template.stats.onRendered(function() {
   this.autorun(function(){
         var topicName = Session.get('topicName');
